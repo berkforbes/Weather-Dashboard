d07c309a2a5f7736e2e1138028bd9b1a7e1c3f1b
berkforbes <berkforbes@yahoo.com>
2021-12-27 22:44:27 -0700

    fetch api, save city to local storage

--- assets/script.js
+++ assets/script.js
@@ -6,7 +6,7 @@ $("#search-btn").on("click", function(event) {
     console.log(citySearch)
 
     if (citySearch) {
-        // call function to fetch weather API
+        getCityForecast(citySearch);
 
         //Clear search
         $("#city-search").val("");
@@ -14,4 +14,25 @@ $("#search-btn").on("click", function(event) {
         //alert if invalid or empty search
         alert("Please Enter a Valid City or Zipcode")
     }
+// Store city search to local storage
+localStorage.setItem("city", JSON.stringify());
+
 });
+
+
+// Fetch openweather API
+var getCityForecast = function(city) {
+    var apiUrl = "https://api.openweathermap.org/data/2.5/weather?q=${city}&units=imperial&appid=44671d4dd26dc86b18a7b64bfa869339";
+
+    fetch(apiUrl)
+        .then(function(response) {
+            // request was succesful
+            if (response.ok) {
+                response.json().then(function(data) {
+                    console.log(data);
+                    displayWeather(data, city);
+                });
+            }
+
+        });
+}
